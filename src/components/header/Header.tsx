9f9e06ca2ac4d3b122e21f10d394a50e9724e2f8
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-11-24 18:38:26 +0100

    remove unused codes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,18 +5,9 @@ export const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header__container">
-        {/* <div className="logo-box">
-          <img src="" alt="logo" />
-        </div>
-        <div>
-          <span>add</span>
-          <span>add</span>
-          <span>add</span>
-        </div> */}
-
         <div className="user-box">
           <h1>Hello, Emmanuel</h1>
-          <p>It's a good day to be tasy'</p>
+          <p>It's a good day to be tasy</p>
         </div>
         <form className="task-function" onSubmit={(e) => e.preventDefault()}>
           <input type="text" placeholder="Search tasks" />
